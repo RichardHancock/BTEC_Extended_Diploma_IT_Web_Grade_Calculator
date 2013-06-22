9f4c3ef5b61b1294c56181073dc13af3d17f9b6c
Richard Hancock <ygrichman@mail.com>
2013-06-22 23:47:07 +0100

    First Working Version
    Now correctly shows the calculated grade
    Saves form data between page refreshes.

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 </head>
 <body>
 	<h1>BTEC Calculator</h1>
-	<?php if ($_POST['submitted']) {
+	<?php 
+		if ($_POST['submitted']) {
 			
 			$error1 = False;
 			$error2 = False;
@@ -16,27 +17,27 @@
 			$merits = 0;
 			$passes = 0;
 			$BTECPoints = 0;
-			$letterGrade = "PPP";
+			$letterGrade = "Failed";
 
 			$grades = array(
-				"1" => $_POST["grade1"], 
-				"2" => $_POST["grade2"], 
-				"3" => $_POST["grade3"], 
-				"4" => $_POST["grade4"],
-				"5" => $_POST["grade5"], 
-				"6" => $_POST["grade6"], 
-				"7" => $_POST["grade7"], 
-				"8" => $_POST["grade8"], 
-				"9" => $_POST["grade9"], 
-				"10" => $_POST["grade10"], 
-				"11" => $_POST["grade11"], 
-				"12" => $_POST["grade12"], 
-				"13" => $_POST["grade13"], 
-				"14" => $_POST["grade14"], 
-				"15" => $_POST["grade15"], 
-				"16" => $_POST["grade16"], 
-				"17" => $_POST["grade17"], 
-				"18" => $_POST["grade18"], 
+				$_POST["grade1"], 
+				$_POST["grade2"], 
+				$_POST["grade3"], 
+				$_POST["grade4"],
+				$_POST["grade5"], 
+				$_POST["grade6"], 
+				$_POST["grade7"], 
+				$_POST["grade8"], 
+				$_POST["grade9"], 
+				$_POST["grade10"], 
+				$_POST["grade11"], 
+				$_POST["grade12"], 
+				$_POST["grade13"], 
+				$_POST["grade14"], 
+				$_POST["grade15"], 
+				$_POST["grade16"], 
+				$_POST["grade17"], 
+				$_POST["grade18"], 
 			);
 			foreach ($grades as $i) {
 				if(is_string($i) == False) {
@@ -64,12 +65,12 @@
 				$BTECPoints += (($passes * 7) * 10);
 			}
 
-			if($error3 = False) {
+			if($error3 == False) {
 				if($BTECPoints >= 1260 and $BTECPoints <= 1299) {
 					$letterGrade = "PPP";
 				} elseif($BTECPoints >= 1300 and $BTECPoints <= 1339) {
 					$letterGrade = "MPP";
-				}  elseif($BTECPoints >= 1340 and $BTECPoints <= 1379) {
+				} elseif($BTECPoints >= 1340 and $BTECPoints <= 1379) {
 					$letterGrade = "MMP";
 				} elseif($BTECPoints >= 1380 and $BTECPoints <= 1419) {
 					$letterGrade = "MMM";
@@ -87,64 +88,66 @@
 					$letterGrade = "D*D*D*";
 				}
 			}
-		} ?>
+		}
+	?>
+		<span><?php echo $BTECPoints; ?></span>
 	<form method="post" action="index.php">
 		<label>Module 1: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade1">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade1']) ? $_POST['grade1'] : 'P' ?>" size="1" name="grade1">
 		<br>
 		<label>Module 2: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade2">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade2']) ? $_POST['grade2'] : 'P' ?>" size="1" name="grade2">
 		<br>
 		<label>Module 3: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade3">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade3']) ? $_POST['grade3'] : 'P' ?>" size="1" name="grade3">
 		<br>
 		<label>Module 4: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade4">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade4']) ? $_POST['grade4'] : 'P' ?>" size="1" name="grade4">
 		<br>
 		<label>Module 5: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade5">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade5']) ? $_POST['grade5'] : 'P' ?>" size="1" name="grade5">
 		<br>
 		<label>Module 6: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade6">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade6']) ? $_POST['grade6'] : 'P' ?>" size="1" name="grade6">
 		<br>
 		<label>Module 7: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade7">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade7']) ? $_POST['grade7'] : 'P' ?>" size="1" name="grade7">
 		<br>
 		<label>Module 8: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade8">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade8']) ? $_POST['grade8'] : 'P' ?>" size="1" name="grade8">
 		<br>
 		<label>Module 9: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade9">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade9']) ? $_POST['grade9'] : 'P' ?>" size="1" name="grade9">
 		<br>
 		<label>Module 10: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade10">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade10']) ? $_POST['grade10'] : 'P' ?>" size="1" name="grade10">
 		<br>
 		<label>Module 11: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade11">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade11']) ? $_POST['grade11'] : 'P' ?>" size="1" name="grade11">
 		<br>
 		<label>Module 12: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade12">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade12']) ? $_POST['grade12'] : 'P' ?>" size="1" name="grade12">
 		<br>
 		<label>Module 13: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade13">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade13']) ? $_POST['grade13'] : 'P' ?>" size="1" name="grade13">
 		<br>
 		<label>Module 14: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade14">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade14']) ? $_POST['grade14'] : 'P' ?>" size="1" name="grade14">
 		<br>
 		<label>Module 15: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade15">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade15']) ? $_POST['grade15'] : 'P' ?>" size="1" name="grade15">
 		<br>
 		<label>Module 16: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade16">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade16']) ? $_POST['grade16'] : 'P' ?>" size="1" name="grade16">
 		<br>
 		<label>Module 17: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade17">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade17']) ? $_POST['grade17'] : 'P' ?>" size="1" name="grade17">
 		<br>
 		<label>Module 18: </label>
-		<input type="text" class="grade" value="P" size="1" name="grade18">
+		<input type="text" class="grade" value="<?php echo isset($_POST['grade18']) ? $_POST['grade18'] : 'P' ?>" size="1" name="grade18">
 		<br>
 		<input type="hidden" name="submitted" value=1>
 		<input type="submit" class="submit">
-		<span><?php echo("Grade: " . $letterGrade); ?></span>
+		<span><?php echo $letterGrade; ?></span>
 </body>
 </html>
